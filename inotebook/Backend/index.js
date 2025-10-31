cb92e6894a49b9630384a803567e9fd5fab7c35d
Aaditya Prajapati <abhiprajapati84494@gmail.com>
2025-10-31 16:33:06 +0530

    url updated

--- inotebook/Backend/index.js
+++ inotebook/Backend/index.js
@@ -6,7 +6,7 @@ const connectToMongo = require("./db");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || "https://full-stack-inotebook.netlify.app/login";
 
 app.use(cors());
 app.use(express.json());
